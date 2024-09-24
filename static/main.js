79bfc81510bd72b9e4b06bfb77550aecdc5cb4fd
hoacoder1 <hoa2k4ne@gmail.com>
2024-09-24 22:07:26 +0700

    Updated

--- static/main.js
+++ static/main.js
@@ -1,59 +1,60 @@
 document.querySelectorAll('.form-check-input').forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-      filterProductsByPrice();
-    });
+  checkbox.addEventListener('change', function() {
+    filterProductsByPrice();
   });
-  
-  function filterProductsByPrice() {
-    // Lấy tất cả sản phẩm
-    const products = document.querySelectorAll('.card.col-lg-3.col-md-4.col-sm-6');
-    
-    // Lấy các checkbox đã được chọn
-    const selectedRanges = Array.from(document.querySelectorAll('.form-check-input:checked')).map(cb => cb.value);
-  
-    // Duyệt qua từng sản phẩm và ẩn/hiện theo giá
-    products.forEach(function(product) {
-      const productPrice = parseInt(product.getAttribute('data-price')); // Lấy giá của sản phẩm
-      let isVisible = false; // Mặc định ẩn sản phẩm
-  
-      // Duyệt qua các khoảng giá đã chọn
-      selectedRanges.forEach(function(range) {
-        const [min, max] = range.split('-').map(Number); // Tách khoảng giá thành min và max
-        if (productPrice >= min * 1000 && productPrice <= max * 1000) {
-          isVisible = true; // Nếu giá nằm trong khoảng, hiển thị sản phẩm
-        }
-      });
-  
-      // Hiển thị hoặc ẩn sản phẩm
-      product.style.display = isVisible || selectedRanges.length === 0 ? 'block' : 'none';
-    });
-  }
+});
 
-  document.querySelectorAll('.form-check-input').forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-      filterProductsByGenres();
+function filterProductsByPrice() {
+  // Lấy tất cả sản phẩm
+  const products = document.querySelectorAll('.card.col-lg-3.col-md-4.col-sm-6');
+  
+  // Lấy các checkbox đã được chọn
+  const selectedRanges = Array.from(document.querySelectorAll('.form-check-input:checked')).map(cb => cb.value);
+
+  // Duyệt qua từng sản phẩm và ẩn/hiện theo giá
+  products.forEach(function(product) {
+    const productPrice = parseInt(product.getAttribute('data-price')); // Lấy giá của sản phẩm
+    let isVisible = false; // Mặc định ẩn sản phẩm
+
+    // Duyệt qua các khoảng giá đã chọn
+    selectedRanges.forEach(function(range) {
+      const [min, max] = range.split('-').map(Number); // Tách khoảng giá thành min và max
+      if (productPrice >= min * 1000 && productPrice <= max * 1000) {
+        isVisible = true; // Nếu giá nằm trong khoảng, hiển thị sản phẩm
+      }
     });
+
+    // Hiển thị hoặc ẩn sản phẩm
+    product.style.display = isVisible || selectedRanges.length === 0 ? 'block' : 'none';
   });
-  
-  function filterProductsByGenres() {
-    const productsg = document.querySelectorAll('.card.col-lg-3.col-md-4.col-sm-6');
-    const selectedGenres = Array.from(document.querySelectorAll('.form-check-input:checked')).map(cb => cb.value);
-  
-    productsg.forEach(function(productg) {
-      const productGenres = productg.getAttribute('data-genres'); 
-      let isVisibleg = false; 
-  
-      selectedGenres.forEach(function(rangeg) {
-        const genresArray = rangeg.split('-');
-        if (genresArray.includes(productGenres)) {
-          isVisibleg = true; 
-        }
-      });
-  
-     
-      productg.style.display = isVisibleg || selectedGenres.length === 0 ? 'block' : 'none';
+}
+
+document.querySelectorAll('.form-check-input-g').forEach(function(checkboxg) {
+  checkboxg.addEventListener('change', function() {
+    filterProductsByGenres();
+  });
+});
+
+function filterProductsByGenres() {
+  const productsg = document.querySelectorAll('.card.col-lg-3.col-md-4.col-sm-6');
+  const selectedGenres = Array.from(document.querySelectorAll('.form-check-input-g:checked')).map(cb => cb.value);
+
+  productsg.forEach(function(productg) {
+    const productGenres = productg.getAttribute('data-genres'); 
+    let isVisibleg = false; 
+
+    selectedGenres.forEach(function(rangeg) {
+      const genresArray = rangeg.split('-');
+      if (genresArray.includes(productGenres)) {
+        isVisibleg = true; 
+      }
     });
-  }
+
+   
+    productg.style.display = isVisibleg || selectedGenres.length === 0 ? 'block' : 'none';
+  });
+}
+
 
 
 
